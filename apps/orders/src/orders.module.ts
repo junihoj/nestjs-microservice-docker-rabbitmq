e90db7a5525a11c4992e0eee3ec5248ff5abaa4d
junihoj <ezeonyekachukwu98@gmail.com>
2024-06-26 08:55:35 +0100

    orders app mono repo

---
+++ apps/orders/src/orders.module.ts
@@ -0,0 +1,25 @@
+import { Module } from '@nestjs/common';
+import {ConfigModule } from '@nestjs/config';
+import { OrdersController } from './orders.controller';
+import { OrdersService } from './orders.service';
+import * as Joi from 'joi';
+import { DatabaseModule } from '@app/common';
+import { MongooseModule } from '@nestjs/mongoose';
+
+
+@Module({
+  imports: [ ConfigModule.forRoot({
+    isGlobal: true,
+    validationSchema: Joi.object({
+      MONGODB_URI: Joi.string().required(),
+      PORT: Joi.number().required(),
+    }),
+    envFilePath: './apps/orders/.env',
+  }),
+  DatabaseModule,
+  // MongooseModule.forFeature([{ name: Order.name, schema: OrderSchema }]),
+],
+  controllers: [OrdersController],
+  providers: [OrdersService],
+})
+export class OrdersModule {}
