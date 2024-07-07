a6339057bc86872535b8464e16947588a23c0f8e
junihoj <ezeonyekachukwu98@gmail.com>
2024-07-07 15:55:11 +0100

    working but no auto-reload on dev

--- apps/orders/src/orders.controller.ts
+++ apps/orders/src/orders.controller.ts
@@ -1,8 +1,19 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post, Req, UseGuards } from '@nestjs/common';
 import { OrdersService } from './orders.service';
+import { JwtAuthGuard } from '@app/common';
+import { CreateOrderRequest } from './dto/create-order.request';
 
-@Controller()
+@Controller('orders')
 export class OrdersController {
   constructor(private readonly ordersService: OrdersService) {}
+  @Post()
+  @UseGuards(JwtAuthGuard)
+  async createOrder(@Body() request: CreateOrderRequest, @Req() req: any) {
+    return this.ordersService.createOrder(request, req.cookies?.Authentication);
+  }
 
+  @Get()
+  async getOrders() {
+    return this.ordersService.getOrders();
+  }
 }
